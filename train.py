6dc3c7b442c0c2e5f9bbef0ca86098ec49300648
Carlos Alzate <carlos-alzate@hotmail.com>
2020-05-03 17:12:59 -0500

    Trying models

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn import linear_model
+from sklearn.ensemble import RandomForestRegressor
 from joblib import dump
 from preprocess import prep_data
 
@@ -7,7 +8,13 @@ df = pd.read_csv("fish_participant.csv")
 
 X, y = prep_data(df)
 
-model = LinearRegression()
+# model = linear_model.LinearRegression() # 5559 - 0.036
+# model = linear_model.Lasso(alpha=0.1) # 5561 - 0.071
+# model = linear_model.Ridge(alpha=0.1, fit_intercept=False) # 5457 - 0.047
+# model = linear_model.OrthogonalMatchingPursuit(n_nonzero_coefs=4) # 5104 - 0.036
+
+model = RandomForestRegressor(criterion="mse") #580 - 0.003
+
 model.fit(X, y)
 
 dump(model, "reg.joblib")
